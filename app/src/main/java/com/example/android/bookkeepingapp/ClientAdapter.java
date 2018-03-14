08ecb942cd96675f4c36486396664582561a9ae8
gamesMum <rasha_gh22@yahoo.com>
2018-03-14 22:53:57 +0300

    just testing

--- app/src/main/java/com/example/android/bookkeepingapp/ClientAdapter.java
+++ app/src/main/java/com/example/android/bookkeepingapp/ClientAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.bookkeepingapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,21 +8,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+import java.util.List;
+
 
 /**
  * Created by Rasha on 14/03/2018.
  */
-
-
 public class ClientAdapter extends ArrayAdapter<Client> {
-    String  checking;
-    public ClientAdapter(@NonNull Context context, int resource) {
-        super(context, resource);
+    public ClientAdapter(@NonNull Context context, int resource, List<Client> objects) {
+        super(context, resource, objects);
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        if (convertView == null) {
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.client_item, parent, false);
+        }
+        //Find the current object
+        Client client = getItem(position);
+        //Do the rest
+        return convertView;
     }
 }
